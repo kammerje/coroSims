b1fe9ee9b996cc066b49414b18fd3959fd8aafb6
Jens Kammerer <jkammerer@tastrock2021.local.stsci.edu>
2023-05-24 15:26:06 -0400

    Version 0.1.2: fixing bug for Ntime == 1

--- load_scene_jens.py
+++ load_scene_jens.py
@@ -53,33 +53,32 @@ class scene():
         # Load star.
         self.angdiam = hdul[3].header['ANGDIAM'] # mas
         self.pixscale = hdul[3].header['PXSCLMAS'] # mas
+        xystar = np.array(temp[:, 1:3]) # pix
+        xystar_x_interp = sp.interpolate.interp1d(times, xystar[:, 0], kind='cubic')
+        xystar_y_interp = sp.interpolate.interp1d(times, xystar[:, 1], kind='cubic')
+        self.xystar = np.array([xystar_x_interp(self.time), xystar_y_interp(self.time)]).T # pix
         if (self.Ntime == 1):
-            self.xystar = np.array(temp[0, 1:3])[np.newaxis, :] # pix
-            self.bposstar = np.array(temp[0, 9:12])[np.newaxis, :] # au
-            self.bvelstar = np.array(temp[0, 12:15])[np.newaxis, :] # au/yr
-            fstar = np.array(temp[0, 15:])[np.newaxis, :] # Jy
-            fstar_interp = sp.interpolate.interp1d(waves, fstar[0], kind='cubic')
-            self.fstar = fstar_interp(self.wave)[np.newaxis, :] # Jy
-            self.fstar_V = fstar_interp(0.540) # Jy, Johnson V-band
-        else:
-            xystar = np.array(temp[:, 1:3]) # pix
-            xystar_x_interp = sp.interpolate.interp1d(times, xystar[:, 0], kind='cubic')
-            xystar_y_interp = sp.interpolate.interp1d(times, xystar[:, 1], kind='cubic')
-            self.xystar = np.array([xystar_x_interp(self.time), xystar_y_interp(self.time)]).T # pix
-            bposstar = np.array(temp[:, 9:12]) # au
-            bposstar_x_interp = sp.interpolate.interp1d(times, bposstar[:, 0], kind='cubic')
-            bposstar_y_interp = sp.interpolate.interp1d(times, bposstar[:, 1], kind='cubic')
-            bposstar_z_interp = sp.interpolate.interp1d(times, bposstar[:, 2], kind='cubic')
-            self.bposstar = np.array([bposstar_x_interp(self.time), bposstar_y_interp(self.time), bposstar_z_interp(self.time)]).T # au
-            bvelstar = np.array(temp[:, 12:15]) # au/yr
-            bvelstar_x_interp = sp.interpolate.interp1d(times, bvelstar[:, 0], kind='cubic')
-            bvelstar_y_interp = sp.interpolate.interp1d(times, bvelstar[:, 1], kind='cubic')
-            bvelstar_z_interp = sp.interpolate.interp1d(times, bvelstar[:, 2], kind='cubic')
-            self.bvelstar = np.array([bvelstar_x_interp(self.time), bvelstar_y_interp(self.time), bvelstar_z_interp(self.time)]).T # au/yr
-            fstar = np.array(temp[:, 15:]) # Jy
-            fstar_interp = sp.interpolate.interp2d(waves, times, fstar, kind='quintic')
-            self.fstar = fstar_interp(self.wave, self.time) # Jy
-            self.fstar_V = fstar_interp(0.540, 0.) # Jy, Johnson V-band
+            self.xystar = self.xystar[np.newaxis, :]
+        bposstar = np.array(temp[:, 9:12]) # au
+        bposstar_x_interp = sp.interpolate.interp1d(times, bposstar[:, 0], kind='cubic')
+        bposstar_y_interp = sp.interpolate.interp1d(times, bposstar[:, 1], kind='cubic')
+        bposstar_z_interp = sp.interpolate.interp1d(times, bposstar[:, 2], kind='cubic')
+        self.bposstar = np.array([bposstar_x_interp(self.time), bposstar_y_interp(self.time), bposstar_z_interp(self.time)]).T # au
+        if (self.Ntime == 1):
+            self.bposstar = self.bposstar[np.newaxis, :]
+        bvelstar = np.array(temp[:, 12:15]) # au/yr
+        bvelstar_x_interp = sp.interpolate.interp1d(times, bvelstar[:, 0], kind='cubic')
+        bvelstar_y_interp = sp.interpolate.interp1d(times, bvelstar[:, 1], kind='cubic')
+        bvelstar_z_interp = sp.interpolate.interp1d(times, bvelstar[:, 2], kind='cubic')
+        self.bvelstar = np.array([bvelstar_x_interp(self.time), bvelstar_y_interp(self.time), bvelstar_z_interp(self.time)]).T # au/yr
+        if (self.Ntime == 1):
+            self.bvelstar = self.bvelstar[np.newaxis, :]
+        fstar = np.array(temp[:, 15:]) # Jy
+        fstar_interp = sp.interpolate.interp2d(waves, times, fstar, kind='quintic')
+        self.fstar = fstar_interp(self.wave, self.time) # Jy
+        if (self.Ntime == 1):
+            self.fstar = self.fstar[np.newaxis, :]
+        self.fstar_V = fstar_interp(0.540, 0.) # Jy, Johnson V-band
         self.dstar = hdul[3].header['DIST'] # pc
         self.Lstar = hdul[3].header['LSTAR'] # Lsun
         self.Vstar = hdul[3].header['M_V'] # absmag
@@ -95,33 +94,28 @@ class scene():
             temp = hdul[4+i].data
             if (len(temp.shape) == 1):
                 temp = temp[np.newaxis, :]
+            xyplanet_x_interp = sp.interpolate.interp1d(times, temp[:, 1], kind='cubic')
+            xyplanet_y_interp = sp.interpolate.interp1d(times, temp[:, 2], kind='cubic')
+            self.xyplanet[i] = np.array([xyplanet_x_interp(self.time), xyplanet_y_interp(self.time)]).T # pix
+            orbplanet_a_interp = sp.interpolate.interp1d(times, temp[:, 3], kind='cubic')
+            orbplanet_e_interp = sp.interpolate.interp1d(times, temp[:, 4], kind='cubic')
+            orbplanet_i_interp = sp.interpolate.interp1d(times, temp[:, 5], kind='cubic')
+            orbplanet_p_interp = sp.interpolate.interp1d(times, temp[:, 6], kind='cubic')
+            orbplanet_o_interp = sp.interpolate.interp1d(times, temp[:, 7], kind='cubic')
+            orbplanet_m_interp = sp.interpolate.interp1d(times, temp[:, 8], kind='cubic')
+            self.orbplanet[i] = np.array([orbplanet_a_interp(self.time), orbplanet_e_interp(self.time), orbplanet_i_interp(self.time), orbplanet_p_interp(self.time), orbplanet_o_interp(self.time), orbplanet_m_interp(self.time)]).T # sma (au), ecc, inc (deg), pan (deg), aop (deg), man (deg)
+            bposplanet_x_interp = sp.interpolate.interp1d(times, temp[:, 9], kind='cubic')
+            bposplanet_y_interp = sp.interpolate.interp1d(times, temp[:, 10], kind='cubic')
+            bposplanet_z_interp = sp.interpolate.interp1d(times, temp[:, 11], kind='cubic')
+            self.bposplanet[i] = np.array([bposplanet_x_interp(self.time), bposplanet_y_interp(self.time), bposplanet_z_interp(self.time)]).T # au
+            bvelplanet_x_interp = sp.interpolate.interp1d(times, temp[:, 12], kind='cubic')
+            bvelplanet_y_interp = sp.interpolate.interp1d(times, temp[:, 13], kind='cubic')
+            bvelplanet_z_interp = sp.interpolate.interp1d(times, temp[:, 14], kind='cubic')
+            self.bvelplanet[i] = np.array([bvelplanet_x_interp(self.time), bvelplanet_y_interp(self.time), bvelplanet_z_interp(self.time)]).T # au/yr
+            fplanet_interp = sp.interpolate.interp2d(waves, times, np.multiply(temp[:, 15:], fstar), kind='quintic')
             if (self.Ntime == 1):
-                self.xyplanet[i, 0] = temp[0, 1:3] # pix
-                self.orbplanet[i, 0] = temp[0, 3:9] # sma (au), ecc, inc (deg), pan (deg), aop (deg), man (deg)
-                self.bposplanet[i, 0] = temp[0, 9:12] # au
-                self.bvelplanet[i, 0] = temp[0, 12:15] # au/yr
-                fplanet_interp = sp.interpolate.interp1d(waves, np.multiply(temp[0, 15:], fstar[0]), kind='cubic')
-                self.fplanet[i, 0] = fplanet_interp(self.wave)[np.newaxis, :] # Jy
+                self.fplanet[i] = fplanet_interp(self.wave, self.time)[np.newaxis, :] # Jy
             else:
-                xyplanet_x_interp = sp.interpolate.interp1d(times, temp[:, 1], kind='cubic')
-                xyplanet_y_interp = sp.interpolate.interp1d(times, temp[:, 2], kind='cubic')
-                self.xyplanet[i] = np.array([xyplanet_x_interp(self.time), xyplanet_y_interp(self.time)]).T # pix
-                orbplanet_a_interp = sp.interpolate.interp1d(times, temp[:, 3], kind='cubic')
-                orbplanet_e_interp = sp.interpolate.interp1d(times, temp[:, 4], kind='cubic')
-                orbplanet_i_interp = sp.interpolate.interp1d(times, temp[:, 5], kind='cubic')
-                orbplanet_p_interp = sp.interpolate.interp1d(times, temp[:, 6], kind='cubic')
-                orbplanet_o_interp = sp.interpolate.interp1d(times, temp[:, 7], kind='cubic')
-                orbplanet_m_interp = sp.interpolate.interp1d(times, temp[:, 8], kind='cubic')
-                self.orbplanet[i] = np.array([orbplanet_a_interp(self.time), orbplanet_e_interp(self.time), orbplanet_i_interp(self.time), orbplanet_p_interp(self.time), orbplanet_o_interp(self.time), orbplanet_m_interp(self.time)]).T # sma (au), ecc, inc (deg), pan (deg), aop (deg), man (deg)
-                bposplanet_x_interp = sp.interpolate.interp1d(times, temp[:, 9], kind='cubic')
-                bposplanet_y_interp = sp.interpolate.interp1d(times, temp[:, 10], kind='cubic')
-                bposplanet_z_interp = sp.interpolate.interp1d(times, temp[:, 11], kind='cubic')
-                self.bposplanet[i] = np.array([bposplanet_x_interp(self.time), bposplanet_y_interp(self.time), bposplanet_z_interp(self.time)]).T # au
-                bvelplanet_x_interp = sp.interpolate.interp1d(times, temp[:, 12], kind='cubic')
-                bvelplanet_y_interp = sp.interpolate.interp1d(times, temp[:, 13], kind='cubic')
-                bvelplanet_z_interp = sp.interpolate.interp1d(times, temp[:, 14], kind='cubic')
-                self.bvelplanet[i] = np.array([bvelplanet_x_interp(self.time), bvelplanet_y_interp(self.time), bvelplanet_z_interp(self.time)]).T # au/yr
-                fplanet_interp = sp.interpolate.interp2d(waves, times, np.multiply(temp[:, 15:], fstar), kind='quintic')
                 self.fplanet[i] = fplanet_interp(self.wave, self.time) # Jy
         
         # Load disk.
